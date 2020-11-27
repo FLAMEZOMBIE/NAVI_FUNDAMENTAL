83a7e6bad686aa014ac10af83d0ddfd48ef6acc0
FLAMEZOMBIE <ulzaparamarta@gmail.com>
2020-11-27 13:49:17 +0700

    NAVI_DRAWER

---
+++ app/src/main/java/com/ulza/navidwr/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.ulza.navidwr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubwayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
